baca22d241e537cf6613e055e68e92b93b95439a
Alexander Varney <alex@varney.me>
2021-06-12 19:00:25 -0400

    add gatsby image and breakpoint image components

---
+++ src/components/atoms/breakpoint-image.tsx
@@ -0,0 +1,48 @@
+import {
+  GatsbyImage,
+  getImage,
+  withArtDirection,
+  GatsbyImageProps,
+  ImageDataLike,
+} from 'gatsby-plugin-image';
+import { BreakpointSizes, Breakpoint } from '../util/config';
+
+interface IBreakpointImage extends Omit<GatsbyImageProps, 'image'> {
+  data: {
+    [x in Breakpoint]?: ImageDataLike;
+  };
+}
+
+const BreakpointImage = ({ data, ...imageProps }: IBreakpointImage) => {
+  if (!data[Breakpoint.base]) {
+    console.error('Did not recieve base size for BreakpointIamge');
+  }
+
+  const base: ImageDataLike = data[Breakpoint.base]!;
+
+  const imagesKeys = Object.keys(data)
+    .filter((key) => key !== Breakpoint.base)
+    .sort(
+      (keyA, keyB) =>
+        (BreakpointSizes.get(keyB as Breakpoint) ?? 0) -
+        (BreakpointSizes.get(keyA as Breakpoint) ?? 0)
+    )
+    .map((key) => {
+      const image = data[key as Breakpoint]!;
+      const minWidth = BreakpointSizes.get(key as Breakpoint);
+
+      return {
+        media: `(min-width: ${minWidth}px)`,
+        image: getImage(image),
+      };
+    })
+    .filter((x) => x.image !== undefined);
+
+  //@ts-ignore
+  const images = withArtDirection(getImage(base), imagesKeys);
+
+  //@ts-ignore
+  return <GatsbyImage image={images} {...imageProps} />;
+};
+
+export default BreakpointImage;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,39 @@ import { css } from '@emotion/react';
 import VC from '../components/atoms/variant-container';
 import Grid from '../components/atoms/grid';
 import Text from '../components/atoms/text';
+import BreakpointImage from '../components/atoms/breakpoint-image';
+import { Breakpoint } from '../components/util/config';
+import { useStaticQuery, graphql } from 'gatsby';
 
 // markup
 const IndexPage = () => {
+  const query = useStaticQuery(graphql`
+    query {
+      image1: file(relativePath: { eq: "unsplash_1.jpg" }) {
+        childImageSharp {
+          gatsbyImageData
+        }
+      }
+      image2: file(relativePath: { eq: "unsplash_2.jpg" }) {
+        childImageSharp {
+          gatsbyImageData
+        }
+      }
+      image3: file(relativePath: { eq: "unsplash_3.jpg" }) {
+        childImageSharp {
+          gatsbyImageData
+        }
+      }
+      image4: file(relativePath: { eq: "unsplash_4.jpg" }) {
+        childImageSharp {
+          gatsbyImageData
+        }
+      }
+    }
+  `);
+
+  console.log(query?.image?.childImageSharp?.gatsbyImageData);
+
   return (
     <VC
       variants={[
@@ -42,38 +72,16 @@ const IndexPage = () => {
             quae unde neque, suscipit enim ipsa fugit reiciendis possimus
             recusandae voluptas.
           </Text>
-          <Text>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora
-            fugiat quo reiciendis quaerat corrupti unde ratione voluptates quas
-            magni facere odit eligendi harum, perspiciatis at ipsum libero
-            ducimus est excepturi. Eaque ratione adipisci eum, voluptas deleniti
-            dolorum vel sint inventore. Fuga delectus, rem ducimus error
-            accusamus ipsa doloremque repudiandae quod voluptatum et, amet,
-            nostrum quas rerum eligendi ullam eos repellat. Consequatur eos
-            autem odit perspiciatis? Ducimus quis quam ut odit voluptatem
-            dolores quo itaque enim neque nostrum rerum sed tempora quod,
-            possimus quia libero quasi distinctio perferendis cum eveniet harum.
-            Atque aliquam recusandae ut totam alias magni harum commodi illo
-            voluptates ipsum, magnam sapiente! Sapiente sequi, maxime id aliquam
-            quae unde neque, suscipit enim ipsa fugit reiciendis possimus
-            recusandae voluptas.
-          </Text>
-          <Text>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora
-            fugiat quo reiciendis quaerat corrupti unde ratione voluptates quas
-            magni facere odit eligendi harum, perspiciatis at ipsum libero
-            ducimus est excepturi. Eaque ratione adipisci eum, voluptas deleniti
-            dolorum vel sint inventore. Fuga delectus, rem ducimus error
-            accusamus ipsa doloremque repudiandae quod voluptatum et, amet,
-            nostrum quas rerum eligendi ullam eos repellat. Consequatur eos
-            autem odit perspiciatis? Ducimus quis quam ut odit voluptatem
-            dolores quo itaque enim neque nostrum rerum sed tempora quod,
-            possimus quia libero quasi distinctio perferendis cum eveniet harum.
-            Atque aliquam recusandae ut totam alias magni harum commodi illo
-            voluptates ipsum, magnam sapiente! Sapiente sequi, maxime id aliquam
-            quae unde neque, suscipit enim ipsa fugit reiciendis possimus
-            recusandae voluptas.
-          </Text>
+          <BreakpointImage
+            data={{
+              [Breakpoint.base]: query.image3?.childImageSharp?.gatsbyImageData,
+              [Breakpoint.md]: query.image2?.childImageSharp?.gatsbyImageData,
+              [Breakpoint.sm]: query.image1?.childImageSharp?.gatsbyImageData,
+              [Breakpoint.lg]: query.image4?.childImageSharp?.gatsbyImageData,
+            }}
+            alt="some test images"
+            imgStyle={{ objectFit: 'contain' }}
+          ></BreakpointImage>
         </VC>
       </Grid.Layout>
     </VC>
